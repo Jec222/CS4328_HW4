c6d39949d20b3c5ec8040324ff4196a475fb1437
Jeremias <jec222@txstate.edu>
2021-04-29 18:59:52 -0500

    comparinf LRU with optimal page replacement

--- hw4.c
+++ hw4.c
@@ -16,20 +16,25 @@ int main(void)
 	
 	printf("Generating referance string ");
 	
-    for(i = 0; i < MAX_REFEREANCE_STRING; ++i){
+    for(i = 0; i < MAX_REFEREANCE_STRING; ++i)
+	{
     	//scanf("%d", &pages[i]);
         pages[i] = rand()%5;
 
     }
-	for(i = 0; i < no_of_frames; ++i){
+	for(i = 0; i < no_of_frames; ++i)
+	{
     	frames[i] = -1;
     }
     
-    for(i = 0; i < MAX_REFEREANCE_STRING; ++i){
+    for(i = 0; i < MAX_REFEREANCE_STRING; ++i)
+	{
     	flag1 = flag2 = 0;
     	
-    	for(j = 0; j < no_of_frames; ++j){
-    		if(frames[j] == pages[i]){
+    	for(j = 0; j < no_of_frames; ++j)
+		{
+    		if(frames[j] == pages[i])
+			{
 	    		counter++;
 	    		time[j] = counter;
 	   			flag1 = flag2 = 1;
@@ -37,9 +42,12 @@ int main(void)
    			}
     	}
     	
-    	if(flag1 == 0){
-			for(j = 0; j < no_of_frames; ++j){
-	    		if(frames[j] == -1){
+    	if(flag1 == 0)
+		{
+			for(j = 0; j < no_of_frames; ++j)
+			{
+	    		if(frames[j] == -1)
+				{
 	    			counter++;
 	    			faults++;
 	    			frames[j] = pages[i];
@@ -50,7 +58,8 @@ int main(void)
     		}	
     	}
     	
-    	if(flag2 == 0){
+    	if(flag2 == 0)
+		{
     		pos = LRU(time, no_of_frames);
     		counter++;
     		faults++;
@@ -60,7 +69,8 @@ int main(void)
     	
     	printf("\n");
     	
-    	for(j = 0; j < no_of_frames; ++j){
+    	for(j = 0; j < no_of_frames; ++j)
+		{
     		printf("%d\t", frames[j]);
     	}
 	}
@@ -69,37 +79,32 @@ int main(void)
 
     int  temp[10],k,max,flag3;
 
-        //int no_of_frames, no_of_pages, frames[10], pages[30], temp[10], flag1, flag2, flag3, i, j, k, pos, max, faults = 0;
-    //printf("Enter number of frames: ");
-    //scanf("%d", &no_of_frames);
-    //
-    //printf("Enter number of pages: ");
-    //scanf("%d", &no_of_pages);
-    //
-    //printf("Enter page reference string: ");
-    //
-    //for(i = 0; i < no_of_pages; ++i){
-    //    scanf("%d", &pages[i]);
-    //}
     faults = 0;
 
-    for(i = 0; i < no_of_frames; ++i){
+    for(i = 0; i < no_of_frames; ++i)
+	{
         frames[i] = -1;
     }
     
-    for(i = 0; i < MAX_REFEREANCE_STRING; ++i){
+    for(i = 0; i < MAX_REFEREANCE_STRING; ++i)
+	{
         flag1 = flag2 = 0;
         
-        for(j = 0; j < no_of_frames; ++j){
-            if(frames[j] == pages[i]){
-                   flag1 = flag2 = 1;
-                   break;
-               }
+        for(j = 0; j < no_of_frames; ++j)
+		{
+            if(frames[j] == pages[i])
+			{
+                flag1 = flag2 = 1;
+                break;
+            }
         }
         
-        if(flag1 == 0){
-            for(j = 0; j < no_of_frames; ++j){
-                if(frames[j] == -1){
+        if(flag1 == 0)
+		{
+            for(j = 0; j < no_of_frames; ++j)
+			{
+                if(frames[j] == -1)
+				{
                     faults++;
                     frames[j] = pages[i];
                     flag2 = 1;
@@ -108,34 +113,43 @@ int main(void)
             }    
         }
         
-        if(flag2 == 0){
+        if(flag2 == 0)
+		{
         	flag3 =0;
         	
-            for(j = 0; j < no_of_frames; ++j){
+            for(j = 0; j < no_of_frames; ++j)
+			{
             	temp[j] = -1;
             	
-            	for(k = i + 1; k < MAX_REFEREANCE_STRING; ++k){
-            		if(frames[j] == pages[k]){
+            	for(k = i + 1; k < MAX_REFEREANCE_STRING; ++k)
+				{
+            		if(frames[j] == pages[k])
+					{
             			temp[j] = k;
             			break;
             		}
             	}
             }
             
-            for(j = 0; j < no_of_frames; ++j){
-            	if(temp[j] == -1){
+            for(j = 0; j < no_of_frames; ++j)
+			{
+            	if(temp[j] == -1)
+				{
             		pos = j;
             		flag3 = 1;
             		break;
             	}
             }
             
-            if(flag3 ==0){
+            if(flag3 ==0)
+			{
             	max = temp[0];
             	pos = 0;
             	
-            	for(j = 1; j < no_of_frames; ++j){
-            		if(temp[j] > max){
+            	for(j = 1; j < no_of_frames; ++j)
+				{
+            		if(temp[j] > max)
+					{
             			max = temp[j];
             			pos = j;
             		}
@@ -148,7 +162,8 @@ int main(void)
         
         printf("\n");
         
-        for(j = 0; j < no_of_frames; ++j){
+        for(j = 0; j < no_of_frames; ++j)
+		{
             printf("%d\t", frames[j]);
         }
     }
